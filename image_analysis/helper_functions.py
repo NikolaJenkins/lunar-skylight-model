7e737d0ee2f757d31d3daf4dc2773fc7b00baee8
Nao Joy <naojoy5@gmail.com>
2026-07-20 16:25:22 -0700

    Created csv to match lunar pit images, added new helper functions to process column 2,3 images

--- image_analysis/helper_functions.py
+++ image_analysis/helper_functions.py
@@ -2,7 +2,8 @@ import argparse
 from pathlib import Path
 import numpy as np
 import cv2
-from osgeo import gdal
+import rasterio
+import random
 
 def valid_dir(path: Path):
     """Use on a provided path to confirm that it's a valid directory"""
@@ -45,31 +46,68 @@ def has_pit(*, cropped_image: np.ndarray, base_image: np.ndarray):
 
     return cropped_image.min() <= shadow_threshold and num_shadowed_pixels >= 10 and is_dense_shadow
 
-def gdal_to_pixel_coords(tiff_path, pit_long, pit_lat):
-    """Obtain pixel coordinates of lunar pit in image with provided latitude and longitude values"""
-    with gdal.Open(tiff_path) as ds:
-        gt = ds.GetGeoTransform()
-        print("Forward Geotransform:", gt)
-        inv_gt = gdal.InvGeoTransform(gt)
-        if inv_gt is None:
-            raise ValueError("The inverse transform didn't succeed.")
-        pixel_coords = gdal.ApplyGeoTransform(inv_gt, pit_long, pit_lat)
-        pit_sample = int(pixel_coords[0])
-        pit_line = int(pixel_coords[1])
-        print(f"The pit is located at sample (x): {pit_sample} and line (y): {pit_line}")
-        return (pit_sample, pit_line)
+def read_raw_img(img_path: str):
+    with rasterio.open(img_path) as dataset:
+        print("Reading file:", img_path)
+        print("Uncompressed grid dimensions:", dataset.width, dataset.height)
+        raw_matrix = dataset.read(1)
+        matrix_32 = raw_matrix.astype(np.float32)
+        avg = matrix_32.mean()
+        std = matrix_32.std()
+        img_min = max(matrix_32.min(), avg - 2 * std)
+        img_max = min(matrix_32.max(), avg + 2 * std)
+        if img_max - img_min > 0:
+            return ((raw_matrix - img_min) / (img_max - img_min) * 255).astype(np.uint8)
+        else:
+            return np.zeros_like(raw_matrix, dtype = np.uint8)
 
-def read_raw_img(img_path: str, width: int):
-    with open(img_path, 'rb') as f:
-        header = f.read(10000)
-        byte_offset = 0
-        end_index = header.find(b'END')
-        if end_index != -1:
-            header_size = 2532 # mandated by LROC SIS
-            byte_offset = ((end_index // header_size) + 1) * 2532
-            print("Found END!")
-    raw_data = np.fromfile(img_path, dtype = np.uint8, offset = byte_offset)
-    print("Did metadata check work?:", len(raw_data) / width)
-    height = len(raw_data) // width
-    raw_data = raw_data[:height * width].reshape((height, width))
-    return raw_data
+def sliding_window_pit_search(
+    img_1,
+    img_2_3,
+    pit_sample,
+    pit_line,
+    window_size = 150,
+    search_radius = 2000,
+):
+    pit_template = img_1[
+        pit_sample - (window_size // 2) : pit_sample + (window_size // 2),
+        pit_line - (window_size // 2) : pit_line + (window_size // 2)
+    ]
+
+    img_height, img_width = img_2_3.shape
+    img_center_x = img_width // 2
+    img_center_y = img_height // 2
+    search_x_min = max(0, img_center_x - search_radius)
+    search_x_max = min(img_width, img_center_x + search_radius)
+    search_y_min = max(0, img_center_y - search_radius)
+    search_y_max = min(img_height, img_center_y + search_radius)
+    search_area = img_2_3[
+        search_x_min: search_x_max,
+        search_y_min : search_y_max
+    ]
+
+    search_area = img_2_3
+    res = cv2.matchTemplate(search_area, pit_template, cv2.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv2.minMaxLoc(res)
+    if max_val > 0.60:
+        tile_match_x, tile_match_y = max_loc
+        col_2_pit_x = tile_match_x + window_size // 2
+        col_2_pit_y = tile_match_y + window_size // 2
+        print(f"Match found! Confidence: {max_val:.4f}, pit pixels: x: {col_2_pit_x}, y: {col_2_pit_y}")
+        return col_2_pit_x, col_2_pit_y
+    print(f"Couldn't find a pit match, highest confidence: {max_val:.4f}")
+    return None, None
+
+def random_crop(img, pit_sample, pit_line, crop_size = 640):
+    offset_radius = crop_size // 2 - 20
+    crop_offset = random.randint(-offset_radius, offset_radius)
+    crop_origin_x = pit_sample + crop_offset
+    crop_origin_y = pit_line + crop_offset
+    cropped_pit_x = 320 - crop_offset
+    cropped_pit_y = 320 - crop_offset
+    crop_radius = crop_size // 2
+    image_crop = img[
+        crop_origin_x - crop_radius : crop_origin_x + crop_radius,
+        crop_origin_y - crop_radius : crop_origin_y + crop_radius
+    ]
+    return image_crop, cropped_pit_x, cropped_pit_y
